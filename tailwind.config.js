f8d8ca675433c3b55e1a26233d6302a4917a79a8
Jairo Saul <JairoProDev@gmail.com>
2025-04-23 10:24:57 -0500

    Refactor project for Next.js integration and Tailwind CSS setup
    - Renamed project from "publicadis" to "publicadis-next" to reflect the transition to Next.js.
    - Updated package.json and package-lock.json to include Next.js, React, and Tailwind CSS as dependencies.
    - Refactored scripts in package.json for Next.js commands.
    - Configured PostCSS to use Tailwind CSS.
    - Updated Tailwind CSS configuration to include paths for pages and components.
    - Added global styles and a custom App component for Next.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx,html}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
